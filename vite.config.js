b9b5a3b193c12b0fc2206144b16528e7128061a8
Rudhi Mediastara <42732580+rudimediaz@users.noreply.github.com>
2022-10-29 00:40:07 +0000

    fix: github preview url

--- vite.config.js
+++ vite.config.js
@@ -11,20 +11,23 @@ export default defineConfig({
 });
 
 function resolveHMR() {
+  const wssOptions = {
+    protocol: "wss",
+    clientPort: 443,
+  };
+
   if (process.env.GITPOD_WORKSPACE_URL) {
     return {
       // removes the protocol and replaces it with the port we're connecting to
       host: process.env.GITPOD_WORKSPACE_URL.replace("https://", `${port}-`),
-      protocol: "wss",
-      clientPort: 443,
+      ...wssOptions,
     };
   }
 
   if (process.env.CODESPACES) {
     return {
-      host: `${process.env.CODESPACE_NAME}-${port}.githubpreview.dev`,
-      protocol: "wss",
-      clientPort: 443,
+      host: `${process.env.CODESPACE_NAME}-${port}.preview.app.github.dev`,
+      ...wssOptions,
     };
   }
 
